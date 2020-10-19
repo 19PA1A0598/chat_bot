e1518155b84019e9b4c606347a8830fff05002ee
Mamidipalli Revanth <60594836+19PA1A0598@users.noreply.github.com>
2020-10-19 12:21:36 +0530

    Update chat bot.py

--- chat bot.py
+++ chat bot.py
@@ -8,10 +8,8 @@ def greeting():
         greetings ="Good Afternoon,"
     elif 17 <= time.hour < 22:
         greetings ="Good Evening,"
-    elif 22 <= time.hour < 24 or 00 <= time.hour < 0o3:
+    elif 22 <= time.hour < 24:
         greetings ="Hi,It's late,"
-    elif 0o3 <= time.hour < 0o5:
-        greetings ="Hi,It's early,"
     intro ="Welcome to ******* Hospitals.I am Alex an reception bot I am here to help you with your doctor appointments,medical needs.May I know your name?"
     print (greetings, intro)
     greets = ["Have a warm welcome.", "It's nice to meet you."]
